0da924d31ddeb7b6d2306b8feb71b558e052b279
Steffen Schröder <st.schroeder@gmail.com>
2017-12-18 21:47:46 +0100

    adding basic structure

--- sak.py
+++ sak.py
@@ -1,7 +1,24 @@
 import click
 
-@click.command()
+@click.group()
 def cli():
-    """Example script."""
-    click.echo('Hello World!')
+    pass
 
+@click.command()
+@click.argument('name')
+def clone(name):
+    click.echo('cloning ' + name)
+
+@click.command()
+@click.argument('name')
+def release(name):
+    click.echo('releasing ' + name)
+
+@click.command()
+@click.argument('name')
+def toggle(name):
+    click.echo('toggling' + name) 
+
+cli.add_command(clone)
+cli.add_command(release)
+cli.add_command(toggle)
